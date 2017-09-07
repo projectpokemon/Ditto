fbd7c9a3f394ff3d5060a05ea79558d8d383f90d
Evan Dixon <evanldixon@live.com>
2017-09-07 10:40:50 -0500

    Add bash startup/shutdown scripts

--- Ditto/Program.cs
+++ Ditto/Program.cs
@@ -20,43 +20,66 @@ namespace Ditto
         {
             _listen = true;
 
+            if (!File.Exists("discord.json"))
+            {
+                Console.WriteLine("Can't find discord.json");
+                return;
+            }
+
+            if (!File.Exists("irc.json"))
+            {
+                Console.WriteLine("Can't find irc.json");
+                return;
+            }
+
             var discordInfo = JsonConvert.DeserializeObject<DiscordConnectionInfo>(File.ReadAllText("discord.json"));
             var ircInfo = JsonConvert.DeserializeObject<IrcConnectionInfo>(File.ReadAllText("irc.json"));
 
             Pair = new ChannelPair(ircInfo, discordInfo);
+            Console.WriteLine("Connecting...");
             Pair.Connect().Wait();
-
-            MainAsync().GetAwaiter().GetResult();
+            Console.WriteLine("Ready.");
+            MainAsync(args).GetAwaiter().GetResult();
         }
 
-        public static async Task MainAsync()
+        public static async Task MainAsync(string[] args)
         {
             // Listen for mannual commands
-            while (_listen)
+            if (args.Contains("noprompt"))
             {
-                var input = Console.ReadLine();
-                var cmd = input.Split(" ".ToCharArray(), 2);
-                switch (cmd[0].ToLower())
+                while (true)
                 {
-                    case "say":
-                        if (cmd.Length > 1)
-                        {
-                            await Pair.SendDiscordMessage(cmd[1]);
-                            Pair.SendIrcMessage(cmd[1]);
-                        }
-                        else
-                        {
-                            Console.WriteLine("Usage: say <message>");
-                        }
-                        break;
-                    case "exit":
-                    case "quit":
-                        _listen = false;
-                        break;
-                    default:
-                        break;
+                    // Block until process is manually stopped
                 }
             }
+            else
+            {
+                while (_listen)
+                {
+                    var input = Console.ReadLine();
+                    var cmd = input.Split(" ".ToCharArray(), 2);
+                    switch (cmd[0].ToLower())
+                    {
+                        case "say":
+                            if (cmd.Length > 1)
+                            {
+                                await Pair.SendDiscordMessage(cmd[1]);
+                                Pair.SendIrcMessage(cmd[1]);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Usage: say <message>");
+                            }
+                            break;
+                        case "exit":
+                        case "quit":
+                            _listen = false;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }            
         }        
     }
 }
---
+++ Ditto/startup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+basePath=$("pwd")
+pidFile="${basePath}/pids.pid"
+
+if [ -f $pidFile ];
+then
+  echo "$pidFile already exists. Stop the process before attempting to start."
+else
+  echo -n "" > $pidFile
+  cd ${basePath}
+  echo "Starting Ditto"
+  dotnet Ditto.dll noprompt 2>&1 > /dev/null &
+  echo -n "$! " >> $pidFile
+fi
